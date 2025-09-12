bb39f99542a79a70ab45b44895681afb6cb5790a
Matthias Schaub <matthias.schaub@heigit.org>
2025-09-12 07:17:36 +0200

    tests: test using vcr.py alongside asyncpg_recorder decorators

---
+++ tests/test_replay_vcrpy.py
@@ -0,0 +1,47 @@
+"""Test interplay between asyncpg-recorder and VCR.py.
+
+Using vcr.py cassette along side asyncpg_recorder cassette resulted in
+indefinitely waiting for testcontainer to be ready. Solved by keeping container
+state in plugin.py.
+"""
+
+import urllib.request
+
+import pytest
+import vcr
+
+from asyncpg_recorder import use_cassette
+from tests import main
+
+pytestmark = pytest.mark.asyncio
+
+
+@use_cassette
+@vcr.use_cassette(path="tests/test_vcr_alongside_asyncpg_recorder.vcr")
+async def test_vcr_alongside_asyncpg_recorder():
+    async def select_version():
+        return await main.select_version_connect_fetch()
+
+    def get_domains():
+        return urllib.request.urlopen("http://www.iana.org/domains/reserved").read()
+
+    db_results = await select_version()
+    assert db_results[0]["server_version"] == "15.13 (Debian 15.13-1.pgdg120+1)"
+    assert db_results[0][0] == "15.13 (Debian 15.13-1.pgdg120+1)"
+    get_domains()
+
+
+# Make sure order of wrapper does not matter
+@vcr.use_cassette(path="tests/test_vcr_alongside_asyncpg_recorder.vcr")
+@use_cassette
+async def test_vcr_alongside_asyncpg_recorder_2():
+    async def select_version():
+        return await main.select_version_connect_fetch()
+
+    def get_domains():
+        return urllib.request.urlopen("http://www.iana.org/domains/reserved").read()
+
+    db_results = await select_version()
+    assert db_results[0]["server_version"] == "15.13 (Debian 15.13-1.pgdg120+1)"
+    assert db_results[0][0] == "15.13 (Debian 15.13-1.pgdg120+1)"
+    get_domains()
