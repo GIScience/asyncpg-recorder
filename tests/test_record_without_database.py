58b7717625f706f9350e0f29153028831b1b037a
Matthias Schaub <matthias.schaub@heigit.org>
2025-12-17 11:42:13 +0100

    tests: fix by using use_cassette decorator to make DSN is set

--- tests/test_record_without_database.py
+++ tests/test_record_without_database.py
@@ -1,10 +1,12 @@
 import pytest
 
+import asyncpg_recorder
 from tests import main
 
 pytestmark = pytest.mark.asyncio
 
 
+@asyncpg_recorder.use_cassette
 async def test_select_now_without_database():
     """No `use_cassette` decorator. Database is not reachable."""
     with pytest.raises(OSError):
